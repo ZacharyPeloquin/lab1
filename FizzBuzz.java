229a4e8731d01fba73926c762cbef135e4078a21
zactrixo <zacharypeloquin@gmail.com>
2020-09-28 11:02:27 -0400

    modification du FizzBuzz pour qu'il s'arrete a 200

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz{
 	public static void main (String args[]){
 		
 		int max = (args.length > 0) ? Integer.parseInt(args[0]): 200;
-		for (int i=1; i<=max; ++i){
+		for (int i=1; i<=200; ++i){
 			if (i%15 == 0) System.out.println("FizzBuzz");
 			else if (i%3 == 0) System.out.println("Fizz");
 			else if (i%5 == 0) System.out.println("Buzz");
